389bc94f82c9e173e35d67aa7c172a0901b67d9e
sasaplus1 <sasaplus1@gmail.com>
2021-06-18 15:29:47 +0900

    add extract.js

---
+++ npm_scripts/extract.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+const cssTree = require('css-tree');
+
+const css = fs.readFileSync(
+  require.resolve('ameba-color-palette.css/ameba-color-palette.css'),
+  'utf8'
+);
+const ast = cssTree.parse(css);
+
+const properties = [];
+
+cssTree.walk(ast, function(node) {
+  const { property, type } = node;
+
+  if (type !== 'Declaration' || !property.startsWith('--')) {
+    return;
+  }
+
+  properties.push(property);
+});
+
+process.stdout.write(
+  properties.join('\n') + '\n'
+);
