4f1c76e54068b693e06d716559acc22c2800d4e9
sasaplus1 <sasaplus1@gmail.com>
2022-11-23 22:36:05 +0900

    add commit tree creating script

---
+++ index.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -euo pipefail
+
+__main() {
+  local -r version="$1"
+
+  if [ "$version" = '' ]
+  then
+    printf -- '%s\n' 'version is empty.' >&2
+    return 1
+  fi
+
+  if [ "$(git tag --list "$version")" != '' ]
+  then
+    return 0
+  fi
+
+  local -r repo=./release
+  local -r dist=./release/dict
+
+  mkdir -p "$dist"
+
+  npm install -D ameba-color-palette.css@"$version"
+
+  node index.js > "$dist/ameba-color-palette.dict"
+
+  git --git-dir="$repo/.git" --work-tree="$repo" add .
+  git --git-dir="$repo/.git" --work-tree="$repo" commit -m release --allow-empty
+  git --git-dir="$repo/.git" --work-tree="$repo" tag "$version"
+}
+__main "$@"
